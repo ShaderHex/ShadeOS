67e3b9d32f178399b9599bc3c48c6d2250b2647b
ShaderHex <barkott9@gmail.com>
2025-10-31 21:37:31 +0300

    Added all characters to keyboard driver

--- core/software/shell.c
+++ core/software/shell.c
@@ -1,7 +1,21 @@
 #include "../../drivers/display.h"
+#include "../../drivers/keyboard.h"
 #include "shell.h"
 
 void shell_loop(void) {
-    clear_screen();
-    print_string("Welcome to the shell!\n");
+    print_string("Welcome to the shell!\n\n");
+    int isRunning = 1;
+
+    while (isRunning) {
+        unsigned char status = port_byte_in(0x64);
+        if (status & 0x01) {
+            unsigned char scancode = keyboard_read_scancode();
+            char ch = keyboard_scancode_to_char(scancode);
+            if (ch != 0) {
+                char str[2] = { ch, 0 };
+                print_string("");
+                print_string(str);
+            }
+        }
+    }
 }
--- drivers/keyboard.c
+++ drivers/keyboard.c
@@ -2,8 +2,8 @@
 
 void init_keyboard() {
     print_string("Initializing keyboard driver...\n");
-    unsigned char scancode = port_byte_in(0x60);
-    print_string("Done initializing keyboard driver!");
+    port_byte_in(0x60);
+    print_string("Done initializing keyboard driver!\n");
 }
 
 unsigned char keyboard_read_scancode() {
@@ -15,6 +15,29 @@ char keyboard_scancode_to_char(unsigned char scancode) {
         case 0x1E: return 'a';
         case 0x30: return 'b';
         case 0x2E: return 'c';
+        case 0x20: return 'd';
+        case 0x12: return 'e';
+        case 0x21: return 'f';
+        case 0x22: return 'g';
+        case 0x23: return 'h';
+        case 0x17: return 'i';
+        case 0x24: return 'j';
+        case 0x25: return 'k';
+        case 0x26: return 'l';
+        case 0x32: return 'm';
+        case 0x31: return 'n';
+        case 0x18: return 'o';
+        case 0x19: return 'p';
+        case 0x10: return 'q';
+        case 0x13: return 'r';
+        case 0x1F: return 's';
+        case 0x14: return 't';
+        case 0x16: return 'u';
+        case 0x2F: return 'v';
+        case 0x11: return 'w';
+        case 0x2D: return 'x';
+        case 0x15: return 'y';
+        case 0x2C: return 'z';
         default: return 0;
     }
 }
